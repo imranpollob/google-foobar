15e0f90a7ed874425c4d8904b7afdfa0b47b79d2
pollmix <polboy777@gmail.com>
2020-07-05 13:37:54 +0600

    solution fixed

--- 4.py
+++ 4.py
@@ -1,36 +1,51 @@
-def with_add(num):
-    steps = 0
-
-    while num > 1:
-        if num % 2 == 0:
-            num = int(num / 2)
-        else:
-            num += 1
-
-        steps += 1
-
-    return steps
-
-
-def with_sub(num):
-    steps = 0
-
-    while num > 1:
-        if num % 2 == 0:
-            num = int(num / 2)
-        else:
-            num -= 1
-
-        steps += 1
-
-    return steps
-
-
 def solution(num):
     num = int(num)
+    count = 0
 
-    return min(with_add(num), with_sub(num))
+    while(num > 1):
+        if (num & 1 == 0):
+            # Dividing even number by two by shifting binary digits one step to the right.
+            num = num >> 1
+        else:
+            a = num + 1
+            b = num - 1
+            # counters ac & bc will be used to count trailing 0s
+            ac = bc = 0
+            # count trailing 0's for num+1
+            while(a & 1 == 0):
+                a = a >> 1
+                ac += 1
+
+            print(num, 'ac', ac)
+
+            # count trailing 0's for num-1
+            while(b & 1 == 0):
+                b = b >> 1
+                bc += 1
+
+            print(num, 'bc', bc)
+
+            # go with num+1 if it has more trailing 0s in binary format. Exception is number 3 as b10 can be divided in less steps than b100.
+            # edge case 3 identified by manually testing numbers 1-10.
+            if (ac > bc and num != 3):
+                num += 1
+            else:
+                num -= 1
+        count += 1
+
+    return count
 
 
-print(solution(15))
-print(solution(4))
+print('Solution - ', solution(15))
+print('-----')
+print('Solution - ', solution(100))
+print('-----')
+print('Solution - ', solution(4))
+print('-----')
+print('Solution - ', solution(3))
+print('-----')
+print('Solution - ', solution(2))
+print('-----')
+print('Solution - ', solution(1))
+print('-----')
+print('Solution - ', solution(0))
